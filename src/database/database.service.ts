55a02b8a98cd3cb8fbde0161d3382c545752ed84
Pablo Daniel Becerra <pablodb1250@gmail.com>
2019-12-24 17:37:14 -0300

    configuration of the database connection

---
+++ src/database/database.service.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+
+export const databaseProvider = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(configService: ConfigService) {
+      return {
+        // ssl: true,
+        pport: 3306,
+        type: 'mysql' as 'mysql',
+        host: configService.get('HOST'),
+        username: configService.get('USER'),
+        password: configService.get('PASSWORD'),
+        database: configService.get('NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      } as ConnectionOptions;
+    },
+  }),
+];
